291919fa9c6a26e594ad1edffb2828ad58907394
Huizi Hu <uceih04@ucl.ac.uk>
2022-04-22 19:07:56 +0000

    update schema

--- Schema.sql
+++ Schema.sql
@@ -3,15 +3,15 @@ DROP SCHEMA IF EXISTS fuel_and_stock_price CASCADE;
 Create schema fuel_and_stock_price;
 
 
-drop table if exists stock_price CASCADE;
-drop table if exists fuel_price CASCADE;
-drop table if exists us_info CASCADE;
-drop table if exists uk_info CASCADE;
-drop table if exists us_diff CASCADE;
-drop table if exists uk_diff CASCADE;
+drop table if exists fuel_and_stock_price.stock_price CASCADE;
+drop table if exists fuel_and_stock_price.fuel_price CASCADE;
+drop table if exists fuel_and_stock_price.us_info CASCADE;
+drop table if exists fuel_and_stock_price.uk_info CASCADE;
+drop table if exists fuel_and_stock_price.us_diff CASCADE;
+drop table if exists fuel_and_stock_price.uk_diff CASCADE;
 
 
-CREATE TABLE "stock_price" (
+CREATE TABLE "fuel_and_stock_price.stock_price" (
   "Date" varchar PRIMARY KEY,
   "uga_Close" float,
   "shell_Close" float,
@@ -21,7 +21,7 @@ CREATE TABLE "stock_price" (
   "shell_Difference" float
 );
 
-CREATE TABLE "fuel_price" (
+CREATE TABLE "fuel_and_stock_price.fuel_price" (
   "Date" varchar PRIMARY KEY,
   "us_price" float,
   "uk_price" float,
@@ -29,7 +29,7 @@ CREATE TABLE "fuel_price" (
   "uk_diff" float
 );
 
-CREATE TABLE "us_info" (
+CREATE TABLE "fuel_and_stock_price.us_info" (
   "Date" varchar PRIMARY KEY,
   "uga_Close" float,
   "uga_Volume" float,
@@ -37,7 +37,7 @@ CREATE TABLE "us_info" (
   "us_price" float
 );
 
-CREATE TABLE "uk_info" (
+CREATE TABLE "fuel_and_stock_price.uk_info" (
   "Date" varchar PRIMARY KEY,
   "shell_Close" float,
   "shell_Volume" float,
@@ -45,24 +45,24 @@ CREATE TABLE "uk_info" (
   "uk_price" float
 );
 
-CREATE TABLE "us_diff" (
+CREATE TABLE "fuel_and_stock_price.us_diff" (
   "Date" varchar PRIMARY KEY,
   "us_price_diff" float,
   "uga_Difference" float
 );
 
-CREATE TABLE "uk_diff" (
+CREATE TABLE "fuel_and_stock_price.uk_diff" (
   "Date" varchar PRIMARY KEY,
   "uk_diff" float,
   "shell_Difference" float
 );
 
-ALTER TABLE "stock_price" ADD FOREIGN KEY ("Date") REFERENCES "us_diff" ("Date");
+ALTER TABLE "fuel_and_stock_price.stock_price" ADD FOREIGN KEY ("Date") REFERENCES "fuel_and_stock_price.us_diff" ("Date");
 
-ALTER TABLE "stock_price" ADD FOREIGN KEY ("Date") REFERENCES "fuel_price" ("Date");
+ALTER TABLE "fuel_and_stock_price.stock_price" ADD FOREIGN KEY ("Date") REFERENCES "fuel_and_stock_price.fuel_price" ("Date");
 
-ALTER TABLE "us_info" ADD FOREIGN KEY ("Date") REFERENCES "stock_price" ("Date");
+ALTER TABLE "fuel_and_stock_price.us_info" ADD FOREIGN KEY ("Date") REFERENCES "fuel_and_stock_price.stock_price" ("Date");
 
-ALTER TABLE "stock_price" ADD FOREIGN KEY ("Date") REFERENCES "uk_info" ("Date");
+ALTER TABLE "fuel_and_stock_price.stock_price" ADD FOREIGN KEY ("Date") REFERENCES "fuel_and_stock_price.uk_info" ("Date");
 
-ALTER TABLE "stock_price" ADD FOREIGN KEY ("Date") REFERENCES "uk_diff" ("Date");
+ALTER TABLE "fuel_and_stock_price.stock_price" ADD FOREIGN KEY ("Date") REFERENCES "fuel_and_stock_price.uk_diff" ("Date");
